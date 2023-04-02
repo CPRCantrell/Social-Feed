895c1311d004c9138a32d63a23291afbab29d1a0
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-04-02 03:09:04 -0400

    📦 NEW: When Item is missing on form, changes border red

--- src/Components/CreatePostForm/CreatePostForm.jsx
+++ src/Components/CreatePostForm/CreatePostForm.jsx
@@ -5,19 +5,27 @@ const CreatePostForm = (props) => {
 
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
+    const [nameMissing, setNameMissing] = useState(false);
+    const [messageMissing, setMessageMissing] = useState(false);
 
     function handleSubmit(event){
         event.preventDefault()
         if(name !== '' && message !== ''){
             props.newPost({name: name, message: message})
             setName('')
+            setNameMissing(false)
             setMessage('')
+            setMessageMissing(false)
         }
         else{
-            let alertMessage = 'Please Input: '
-            if(name === '' && message === ''){alertMessage += 'Name and Message'}
-            else if (name === ''){alertMessage += 'Name'}
-            else{alertMessage += 'Message'}
+            setNameMissing(false)
+            setMessageMissing(false)
+            if(name === ''){
+                setNameMissing(true)
+            }
+            if(message === ''){
+                setMessageMissing(true)
+            }
         }
     }
 
@@ -25,11 +33,11 @@ const CreatePostForm = (props) => {
         <form onSubmit={handleSubmit} className='form-grid'>
             <div className='form-group'>
                 <label>Name</label>
-                <input type="text" className='form-control' value={name} onChange={(event) => setName(event.target.value)}/>
+                <input type="text" className={`form-control ${nameMissing ? 'error' : ''}`} value={name} onChange={(event) => setName(event.target.value)} onFocus={() => setNameMissing(false)}/>
             </div>
             <div className='form-group need-space'>
                 <label>Thoughts</label>
-                <textarea value={message} className='form-control' onChange={(event) => setMessage(event.target.value)}/>
+                <textarea value={message} className={`form-control ${messageMissing ? 'error' : ''}`} onChange={(event) => setMessage(event.target.value)} onFocus={() => setMessageMissing(false)}/>
             </div>
             <button type="submit" className='btn need-space'>Post Update</button>
         </form>
