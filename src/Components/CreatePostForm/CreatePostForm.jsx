828c0bc6a83e4a99fc4604d75a8633e90dc9477d
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-04-04 11:55:37 -0400

    📦 NEW: Text and messages appear when input is in valid to better see the issue

--- src/Components/CreatePostForm/CreatePostForm.jsx
+++ src/Components/CreatePostForm/CreatePostForm.jsx
@@ -6,39 +6,57 @@ const CreatePostForm = (props) => {
 
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
-    const [nameMissing, setNameMissing] = useState(false);
-    const [messageMissing, setMessageMissing] = useState(false);
+    const [nameInvalid, setNameInvalid] = useState(false);
+    const [messageInvalid, setMessageInvalid] = useState(false);
 
     function handleSubmit(event){
         event.preventDefault()
-        if(name !== '' && message !== ''){
-            let postTime = new Date()
-            postTime = FormatTime(postTime)
-            props.newPost({name: name, message: message, time: postTime})
-            setName('')
-            setMessage('')
+        let postTime = new Date()
+        postTime = FormatTime(postTime)
+        props.newPost({name: name, message: message, time: postTime})
+        setName('')
+        setMessage('')
+    }
+
+    function handleNameBlur(e){
+        if(e.target.value !== ''){
+            setNameInvalid(false)
         }
-        else{
-            setNameMissing(false)
-            setMessageMissing(false)
-            if(name === ''){
-                setNameMissing(true)
-            }
-            if(message === ''){
-                setMessageMissing(true)
-            }
+    }
+
+    function handleMessageBlur(e){
+        if(e.target.value !== ''){
+            setMessageInvalid(false)
         }
+    }
+
+    function handleNameInvalid(e){
+        e.preventDefault()
+        setNameInvalid(true)
+    }
+
+    function handleMessageInvalid(e){
+        e.preventDefault()
+        setMessageInvalid(true)
     }
 
     return (
         <form onSubmit={handleSubmit} className='form-grid'>
+            <h2 className='form-title'>Share Your Thoughts</h2>
+            <div className={`alert-text alert-title ${nameInvalid||messageInvalid ? '' : 'hidden'}`}>required fields must be filled</div>
             <div className='form-group'>
-                <label>Name</label>
-                <input type="text" className={`form-control ${nameMissing ? 'error' : ''}`} value={name} onChange={(event) => setName(event.target.value)} onFocus={() => setNameMissing(false)}/>
+                <div className='input-label'>
+                    <label>Name</label>
+                    <div className={`alert-text ${nameInvalid ? '' : 'hidden'}`}>*required</div>
+                </div>
+                <input type="text" className={`form-control ${nameInvalid ? 'invalid-input' : ''}`} value={name} onChange={(event) => setName(event.target.value)} onBlur={(e) => handleNameBlur(e)} onInvalid={(e) => handleNameInvalid(e)} required/>
             </div>
             <div className='form-group need-space'>
-                <label>Thoughts</label>
-                <textarea value={message} className={`form-control ${messageMissing ? 'error' : ''}`} onChange={(event) => setMessage(event.target.value)} onFocus={() => setMessageMissing(false)}/>
+                <div className='input-label'>
+                    <label>Thoughts</label>
+                    <div className={`alert-text ${messageInvalid ? 'invalid' : 'hidden'}`}>*required</div>
+                </div>
+                <textarea className={`form-control ${messageInvalid ? 'invalid-input' : ''}`} value={message} onChange={(event) => setMessage(event.target.value)} onBlur={(e) => handleMessageBlur(e)} onInvalid={(e) => handleMessageInvalid(e)} required/>
             </div>
             <button type="submit" className='btn need-space'>Post Update</button>
         </form>
